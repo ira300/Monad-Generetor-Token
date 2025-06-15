32d4e742bd1a04fdcbc4661585f83335f1a010d1
Ira <60801603+ira300@users.noreply.github.com>
2025-06-15 19:14:20 -0300

    Update and rename ContractTokenGeneration.sol to Tokengeneration.sol

---
+++ contracts/Tokengeneration.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenFactory is Ownable {
+    event TokenCreated(address indexed tokenAddress, string name, string symbol, uint256 initialSupply);
+
+    constructor() Ownable(msg.sender) {} 
+
+    function createToken(string memory name, string memory symbol, uint256 initialSupply) external {
+        bytes32 salt = keccak256(abi.encodePacked(msg.sender, name, symbol, initialSupply));
+        Token newToken = new Token{salt: salt}(name, symbol, initialSupply, msg.sender);
+        emit TokenCreated(address(newToken), name, symbol, initialSupply);
+    }
+}
+
+contract Token is ERC20, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialSupply, address creator)
+        ERC20(name, symbol)
+        Ownable(creator) 
+    {
+        _mint(creator, initialSupply);
+    }
+}
+
+
